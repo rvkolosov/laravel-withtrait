55c4e9cdf31dbac23ba72935db4b14e1d88bf1c0
69pRomens <roman.kolosov12@yandex.ru>
2019-06-21 15:26:27 +0300

    Init package

---
+++ src/Traits/WithTrait.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace RVKolosov\LaravelWithTrait;
+
+trait WithTrait
+{
+    /**
+     * @param $query
+     * @return mixed
+     */
+    public function scopeWithRelations($query)
+    {
+        if (request()->has('with') && is_array(request()->input('with')))
+        {
+            foreach (request()->input('with') as $relations)
+            {
+                $this->hasRelations($relations);
+
+                $query->with($relations);
+            }
+        }
+
+        return $query;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function scopeLoadRelations()
+    {
+        if (request()->has('with') && is_array(request()->input('with')))
+        {
+            $tmp = [];
+
+            foreach (request()->input('with') as $relations)
+            {
+                $this->hasRelations($relations);
+
+                $tmp[] = $relations;
+            }
+
+            $this->load($tmp);
+        }
+
+        return $this;
+    }
+
+    /**
+     * @param $relations
+     */
+    private function hasRelations($relations)
+    {
+        try
+        {
+            $this->has($relations);
+        }
+        catch (\Exception $exception)
+        {
+            if (!config('app.debug'))
+            {
+                abort(404);
+            }
+        }
+    }
+}
